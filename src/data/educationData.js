3db82fd979cb40452e95e21b05adae651106d325
nawani-rohit <nawani.r@northeastern.edu>
2023-01-16 15:56:46 -0500

    Updates

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,17 +1,17 @@
 export const educationData = [
   {
     id: 1,
-    institution: "Mumbai University",
-    course: "Bachelor of Engineering in Computer Engineering",
-    startYear: "2016",
-    endYear: "2020",
-  },
-  {
-    id: 2,
     institution: "Northeastern University",
     course: "Masters of Science in Information Systems",
     startYear: "2022",
     endYear: "present",
+  },
+  {
+    id: 2,
+    institution: "Mumbai University",
+    course: "Bachelor of Engineering in Computer Engineering",
+    startYear: "2016",
+    endYear: "2020",
   },
   //   {
   //     id: 3,
