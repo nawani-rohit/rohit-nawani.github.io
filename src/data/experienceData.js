7b57df102aa21f34e0c0389122226aeaa85b0f08
nawani-rohit <nawani.r@northeastern.edu>
2023-01-16 23:39:29 -0500

    update

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,14 +1,14 @@
 export const experienceData = [
   {
     id: 1,
-    company: "Tripund Solutions Private Limited",
+    company: "Capgemini Technology Services India Limited",
     jobtitle: "Analyst - Software Developer",
     startYear: "2021",
     endYear: "2022",
   },
   {
     id: 2,
-    company: "Capgemini Technology Services India Limited",
+    company: "Tripund Solutions Private Limited",
     jobtitle: "Web Developer Intern",
     startYear: "2019",
     endYear: "2020",
