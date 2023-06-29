3021cd822a3d3b0fe4aa9b0beb9a61fb6bb694bb
nawani-rohit <nawani.r@northeastern.edu>
2023-06-29 16:10:54 -0400

    Updates

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,3 +1,4 @@
+import image01 from "../assets/png/dutch-auction.PNG";
 import image1 from "../assets/png/NuMoves.png";
 import image2 from "../assets/png/covid19.PNG";
 import image3 from "../assets/png/portfolio.PNG";
@@ -7,6 +8,16 @@ import image5 from "../assets/png/HER.PNG";
 export const projectsData = [
   {
     id: 1,
+    projectName: "Dutch Auction Application",
+    projectDesc:
+      "Implemented a smart contract-based decentralized application using a Dutch Auction mechanism with Solidity and the Hardhat Ethereum environment by integrating a ReactJS front-end user interface for dynamic auction. Demonstrated practical skills in full-stack blockchain development by deploying the DApp on an Ethereum testnet and hosting the UI through IPFS for universal access.",
+    tags: ["React", "CSS", "Javascript", "Node.js", "MongoDB"],
+    code: "https://github.com/nawani-rohit/duction-auction",
+    demo: "https://github.com/nawani-rohit/duction-auction",
+    image: image01,
+  },
+  {
+    id: 2,
     projectName: "NU Moves",
     projectDesc:
       "Developed a comprehensive web platform with user-friendly features to enable Northeastern University students to buy, sell & rent household goods among themselves. NU Moves aims to alleviate the hassle students face every year while moving in and out of their accommodation.",
@@ -16,7 +27,7 @@ export const projectsData = [
     image: image1,
   },
   {
-    id: 2,
+    id: 3,
     projectName: "HER - Heal Empower Relief",
     projectDesc:
       "The project aims to assist and support individuals who have been affected by acid attacks by coordinating and facilitating access to various types of professional assistance, including medical care, legal aid, and rehabilitation services",
@@ -26,7 +37,7 @@ export const projectsData = [
     image: image5,
   },
   {
-    id: 3,
+    id: 4,
     projectName: "Movie Finder",
     projectDesc:
       "A simple web application that allows users to search for movies and view information about them.",
@@ -36,7 +47,7 @@ export const projectsData = [
     image: image4,
   },
   {
-    id: 4,
+    id: 5,
     projectName: "Covid-19 Live Updates",
     projectDesc:
       "Covid-19 Live Updates is the real-time web application which gives real-time update about the corona-virus cases in the world build on ReactJs",
@@ -46,7 +57,7 @@ export const projectsData = [
     image: image2,
   },
   {
-    id: 5,
+    id: 6,
     projectName: "Rohit Nawani - Portfolio",
     projectDesc: "Personal Portfolio ",
     tags: ["React", "CSS", "Javascript"],
