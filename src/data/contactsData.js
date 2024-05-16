cf2d54dae3f11df245539da751627985e75fb8d5
Rohit <114552652+nawani-rohit@users.noreply.github.com>
2024-05-16 16:40:50 -0400

    Updated Resume

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,6 +1,6 @@
 export const contactsData = {
   email: "nawani.r@northeastern.edu",
-  phone: "+1 (857) 294-4435",
+  phone: "+1 (617)-888-2141",
   address: "1171 Boylston St, Boston, MA 02215",
 
   sheetAPI: "",
--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,13 +1,20 @@
 export const experienceData = [
   {
     id: 1,
+    company: "IpserLab",
+    jobtitle: "Software Engineer Intern",
+    startYear: "2024",
+    endYear: "2024",
+  },
+  {
+    id: 2,
     company: "Capgemini Technology Services India Limited",
     jobtitle: "Analyst - Software Developer",
     startYear: "2021",
     endYear: "2022",
   },
   {
-    id: 2,
+    id: 3,
     company: "Tripund Solutions Private Limited",
     jobtitle: "Web Developer Intern",
     startYear: "2019",
